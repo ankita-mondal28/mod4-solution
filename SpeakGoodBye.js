c2d0e83b83c9d7c54432d642fa009f43fcd11562
ankita-mondal28 <ankitamondal2801@gmail.com>
2025-09-25 11:04:02 +0530

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,18 @@
+// STEP 6: Wrap everything inside an IIFE
+(function(window) {
+
+  // STEP 7: Create an object called 'byeSpeaker'
+  var byeSpeaker = {};
+
+  // DO NOT attach the speakWord variable to the 'byeSpeaker' object
+  var speakWord = "Good Bye";
+
+  // STEP 8: Attach the 'speak' function to the byeSpeaker object
+  byeSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 9: Expose 'byeSpeaker' to the global scope
+  window.byeSpeaker = byeSpeaker;
+
+})(window); // <-- immediately invoke the function, passing in 'window'
